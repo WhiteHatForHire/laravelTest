29770c110b490db32d1e9c5174c4db8531fba492
Maxwell Stephens <stephensm2011@gmail.com>
2018-03-20 22:15:47 -0400

    initial commit

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /*
+    index
+    create
+    store
+    edit
+    update
+    show 
+    destroy
+    */
+}
